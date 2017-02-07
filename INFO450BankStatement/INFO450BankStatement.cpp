92ecc8232bb1f11991cae48c4e6b917a4977309e
James Nicholson <nicholsonjb@vcu.edu>
2017-02-07 07:45:25 -0500

    Start of project

--- INFO450BankStatement/INFO450BankStatement.cpp
+++ INFO450BankStatement/INFO450BankStatement.cpp
@@ -2,10 +2,23 @@
 //
 
 #include "stdafx.h"
+#include<conio.h>
+#include<iostream>
+#include<iomanip>
+using namespace std;
 
+class bank
+{
+	float balance;
+	public: 
+		void deposit();
+		void withdraw();
+		void checks();
+};
 
 int main()
 {
+	
     return 0;
 }
 
