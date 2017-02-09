050fbe50f3fd26a97149a44f3328b11bc19c35c7
James Nicholson <nicholsonjb@vcu.edu>
2017-02-09 14:10:11 -0500

    Added Extra Credit

--- INFO450BankStatementExtraCredit/INFO450BankStatementExtraCredit.cpp
+++ INFO450BankStatementExtraCredit/INFO450BankStatementExtraCredit.cpp
@@ -1,11 +1,79 @@
-// INFO450BankStatementExtraCredit.cpp : Defines the entry point for the console application.
+// INFO450BankStatement.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
+#include<conio.h>
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+//declare variables
+float balance;
+float deposits;
+float check;
+float withdraws;
+float totalDeposit = 0;
+float totalWithdrawal = 0;
+float totalCheck = 0;
+float amt;
+char choice; //choice for selection
 
 
 int main()
 {
-    return 0;
-}
+	//enter balance
+	cout << "Please enter your balance : " << endl;
+	cin >> balance;
+
+	do
+	{
+		cout << "Please choose the following transaction options. " << endl;
+		cout << "Enter (W)ithdrawal, (D)eposit, (C)heck, (Q)uit : \n";
+		cin >> choice;
+
+		//switch statement to calculate transactions
+		switch (choice)
+		{
+			//calcuate withdraws
+		case 'W':
+		case 'w':
+			cout << "Please enter the amount you would like to withdraw :" << endl;
+			cin >> amt;
+			totalWithdrawal += amt;
+			break;
+
+			//calcuate deposits
+		case 'D':
+		case 'd':
+			cout << "Please enter the amount you would like to deposit :" << endl;
+			cin >> amt;
+			totalDeposit += amt;
+			break;
+
+			//calcuate checks
+		case 'C':
+		case 'c':
+			cout << "Please enter your check amounts :" << endl;
+			cin >> amt;
+			totalCheck += amt;
+			break;
+			//exits switch statement
+		case 'Q':
+		case 'q':
+			break;
+		default: cout << "Invaild entry! " << endl;
+
+		}
+	} while (choice != 'q' && choice != 'Q');
+	cout << "\nTransactions Summary" << endl;
+	cout << "---------------------\n";
+	cout << "Beginning Balance:  " << fixed << setprecision(2) << balance << endl;
+	cout << "Total Deposits:     " << fixed << setprecision(2) << totalDeposit << endl;
+	cout << "Total Checks:       " << fixed << setprecision(2) << totalCheck << endl;
+	cout << "Total Withdrawals:  " << fixed << setprecision(2) << totalWithdrawal << endl;
+	cout << "---------------------\n";
+	cout << "Ending Balance: " << fixed << setprecision(2) << balance + totalDeposit - totalCheck - totalWithdrawal << endl;
+
+	return 0;
+} //end main
 
